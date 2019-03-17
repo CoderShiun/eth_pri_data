c008436934f3bef5044e2aa7f0496f81164336ef
CoderShiun <123657@gmail.com>
2019-03-17 19:52:52 +0100

    add print data

--- checkTx.go
+++ checkTx.go
@@ -17,4 +17,22 @@ func checkTxbyTxHash(tx *types.Transaction) {
 		log.Fatal(err)
 	}
 	fmt.Println("tx data = ", ozone)
+
+	fmt.Println("Stationscode: ", ozone.Stationscode)
+	fmt.Println("Stationsname: ", ozone.Stationsname)
+	fmt.Println("Tagesmaxima: ", ozone.Tagesmaxima)
+	fmt.Println("Aktuellster Messtag im Jahr:", ozone.Aktuellster_Messtag_im_Jahr)
+	fmt.Println("Erster Messtag im Jahr: ", ozone.Erster_Messtag_im_Jahr)
+	fmt.Print("Jan:", ozone.Jan, ", ")
+	fmt.Print("Feb:", ozone.Feb, ", ")
+	fmt.Print("Mar:", ozone.Mar, ", ")
+	fmt.Print("Apr:", ozone.Apr, ", ")
+	fmt.Print("Mai:", ozone.Mai, ", ")
+	fmt.Println("Jun:", ozone.Jun, ", ")
+	fmt.Print("Jul:", ozone.Jul, ", ")
+	fmt.Print("Aug:", ozone.Aug, ", ")
+	fmt.Print("Sep:", ozone.Sep, ", ")
+	fmt.Print("Okt:", ozone.Okt, ", ")
+	fmt.Print("Nov:", ozone.Nov, ", ")
+	fmt.Print("Dez:", ozone.Dez)
 }
